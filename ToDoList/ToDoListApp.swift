8ba19f50003069b9e93b9fef861b32abbd64d235
goldbilgisayar <ortacmevlude@gmail.com>
2022-01-10 18:33:06 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by MEWO on 10.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
